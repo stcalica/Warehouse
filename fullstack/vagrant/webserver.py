4875a23fd236684acaf9173ec0acb7917d228bbd
stcalica <stcalica@gmail.com>
2015-04-25 09:41:53 -0700

    added new udacity repo

--- fullstack/vagrant/webserver.py
+++ fullstack/vagrant/webserver.py
@@ -1,2 +1,33 @@
 #!/bin/py
 
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class WebServerHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		if self.path.endswith("/hello"):
+			self.send_response(200)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			message = ""
+			message += "<html><body>Hello!</body></html>"
+			self.wfile.write(message)
+			print message
+			return
+		else:
+			self.send_error(404, 'File Not Found: %s' % self.path)
+
+
+def main():
+	try:
+		port = 8080
+		server = HTTPServer(('', port), webserverHandler)
+		print "Webserver running on port %s" % port
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print "^C entered, stopping server..."
+		server.socket.close()
+
+
+#auto runs the script
+if __name__ == '__main__':
+		main()
